201baf7bf53ac934bed3935d928e6ca4663ed23b
xionghuayu <xionghuayu@benmu-health.com>
2018-01-15 09:54:04 +0800

    mod: debugServer Change ip to domain

--- config/eros.native.js
+++ config/eros.native.js
@@ -17,7 +17,7 @@ module.exports = {
         'jsServer': 'http://app.weex-eros.com:8889',
         'image': 'https://lev-inf.benmu-health.com/test/xxx',
         'bundleUpdate': 'http://localhosts:3001/app/check',
-        'debugServer': 'ws://192.168.15.110:8088/debugProxy/native'
+        'debugServer': 'ws://app.weex-eros.com:8088/debugProxy/native'
     },
     'zipFolder': {
         'iOS': '/ios/WeexEros/WeexEros',
