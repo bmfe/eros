c1d5aea47dd18e7d7d0559cf3f9cad22ab768fdd
Mingzhe Yang <yangmingzhe@benmu-health.com>
2018-01-24 18:29:45 +0800

    add local iconfont

--- src/js/pages/demo/index.vue
+++ src/js/pages/demo/index.vue
@@ -90,7 +90,7 @@ export default {
 
         domModule.addRule('fontFace', {
             fontFamily: 'iconfont-eros',
-            src: 'url(\'https://at.alicdn.com/t/font_545350_76nuzg1ff8rm5cdi.ttf\')'
+            'src': 'url(\'bmlocal://iconfont/iconfont-eros.ttf\')'
         });
     },
     created () {
