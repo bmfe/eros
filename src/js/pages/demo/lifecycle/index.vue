504b0107103f3697447e724ef17f87f7d949e32a
Mingzhe Yang <yangmingzhe@benmu-health.com>
2018-01-31 15:24:25 +0800

    mod: add notice trash demo

--- src/js/pages/demo/lifecycle/index.vue
+++ src/js/pages/demo/lifecycle/index.vue
@@ -1,40 +1,75 @@
 <template>
+  <wxc-tab-bar :tab-titles="tabTitles" :tab-styles="tabStyles" title-type="icon" @wxcTabBarCurrentTabSelected="wxcTabBarCurrentTabSelected">
+    <!--The first page content-->
+    <div class="item-container" :style="contentStyle">
+      <text>Home</text>
+    </div>
+
+    <!--The second page content-->
+    <div class="item-container" :style="contentStyle">
+      <text>Hot</text>
+    </div>
+
+    <!-- The third page content-->
+    <div class="item-container" :style="contentStyle">
+      <text>Message</text>
+    </div>
+
+    <!-- The fourth page content-->
+    <div class="item-container" :style="contentStyle">
+      <text>My</text>
+    </div>
+  </wxc-tab-bar>
 </template>
+
 <script>
-if (process.env.NODE_ENV === 'development') require('Config')
+import { WxcTabBar, Utils } from "weex-ui";
+import Config from "./config";
+var modal = weex.requireModule("modal");
 export default {
-    bmRouter: {
-        viewWillAppear (params, options) {
-            console.log('viewWillAppear');
-        },
+  data() {
+    return {
+      tabTitles: Config.tabTitles,
+      tabStyles: Config.tabStyles
+    };
+  },
+  created() {
+    this.$notice.toast({
+      message: "hello" // 展示内容
+    });
+    this.$navigator.setNavigationInfo({
+      statusBarStyle: "LightContent"
+    });
+    const tabPageHeight = Utils.env.getPageHeight();
+    // If the page doesn't have a navigation bar
+    // const tabPageHeight = env.deviceHeight / env.deviceWidth * 750;
+    const { tabStyles } = this;
+    this.contentStyle = { height: tabPageHeight - tabStyles.height + "px" };
+  },
+  components: { WxcTabBar },
+  methods: {
+    wxcTabBarCurrentTabSelected(e) {
+      const index = e.page;
+      console.log(e.page,'###########')
 
-        viewDidAppear (params, options) {
-            console.log('viewDidAppear');
-        },
-
-        viewWillBackAppear (params, options) {
-            console.log('viewDidAppear');
-        },
-
-        viewDidBackAppear (params, options) {
-            console.log('viewDidAppear');
-        },
-
-        viewWillDisappear (options) {
-            console.log('viewDidAppear');
-        },
-
-        viewDidDisappear (options) {
-            console.log('viewDidAppear');
-        }
-    },
-    data () {
-        return {
-
-        }
-    },
-    methods: {
-
+    // FIXME 崩溃
+    //   this.$notice.toast({
+    //     message: index // 展示内容
+    //   });
+      // if (index == 3) {
+      //   this.$navigator.setNavigationInfo({
+      //     statusBarStyle: "LightContent"
+      //   });
+      // }
     }
-}
+  }
+};
 </script>
+<style scoped>
+.item-container {
+  width: 750px;
+  background-color: #f2f3f4;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/js/pages/demo/lifecycle/life.vue
@@ -0,0 +1,43 @@
+<template>
+    <div style="width:750px; padding-top:100px; padding-left:100px;">
+       <text>请查看控制台打印</text>
+    </div>
+</template>
+<script>
+// if (process.env.NODE_ENV === 'development') require('Config')
+export default {
+    bmRouter: {
+        viewWillAppear (params, options) {
+            console.log('viewWillAppear');
+        },
+
+        viewDidAppear (params, options) {
+            console.log('viewDidAppear');
+        },
+
+        viewWillBackAppear (params, options) {
+            console.log('viewDidAppear');
+        },
+
+        viewDidBackAppear (params, options) {
+            console.log('viewDidAppear');
+        },
+
+        viewWillDisappear (options) {
+            console.log('viewDidAppear');
+        },
+
+        viewDidDisappear (options) {
+            console.log('viewDidAppear');
+        }
+    },
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
