95508d08a90dcd50bb0f2b6c9f27237477857a3a
Mingzhe Yang <yangmingzhe@benmu-health.com>
2018-02-01 14:21:54 +0800

    add new script for ios/android install and install

--- scripts/update.ios.sh
+++ scripts/update.ios.sh
@@ -8,16 +8,16 @@ function error_exit {
     echo ""
     echo " Code tips: "
     echo -e "\033[33m1 -> platforms/ios/WeexEros/ is not existed. \033[0m"
-    echo -e "\033[33m2 -> cannot git pull WeexiOSSDK. \033[0m"
-    echo -e "\033[33m3 -> cannot git pull Benmu-iOS-Library. \033[0m"
+    echo -e "\033[33m2 -> cannot git pull platforms/ios/WeexEros/WeexiOSSDK, you can try a manual operation. \033[0m"
+    echo -e "\033[33m3 -> cannot git pull platforms/ios/WeexEros/Benmu-iOS-Library, you can try a manual operation. \033[0m"
     echo -e "\033[33m4 -> pod update failed. \033[0m"
     echo -e "\033[33m5 -> run open xcode failed, make sure you had install xcode in appStore. \033[0m"
     exit 1
 }
 
-cd platforms/ios/WeexEros/ && \
-echo -e "$eros_prefix Enter ios project" || \
+cd platforms/ios/WeexEros/ || \
 error_exit 1
+echo -e "$eros_prefix Enter ios project"
 
 if [ -e WeexiOSSDK/ ] && [ -e Benmu-iOS-Library/ ]
 then
