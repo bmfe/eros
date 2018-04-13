1f6632cce04b7f15385d917d56a57b14f5354844
xionghuayu <xionghuayu@benmu-health.com>
2018-04-13 15:43:11 +0800

    mod: package ErosPluginGeTui Component

--- platforms/ios/WeexEros/WeexEros/AppDelegate.h
+++ platforms/ios/WeexEros/WeexEros/AppDelegate.h
@@ -10,8 +10,7 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
-
+@property (nonatomic, strong) UIWindow *window;
 
 @end
 
--- platforms/ios/WeexEros/WeexEros/AppDelegate.m
+++ platforms/ios/WeexEros/WeexEros/AppDelegate.m
@@ -7,21 +7,17 @@
 //
 
 #import "AppDelegate.h"
-
 #import <BMBaseLibrary/BMConfigManager.h>
 #import <BMBaseLibrary/BMMediatorManager.h>
-#import <BMBaseLibrary/BMPushMessageManager.h>
 
-#import <GTSDK/GeTuiSdk.h>
 #import <WXApi.h>
 
 #import <BMBaseLibrary/BMRouterManager.h>
-#import <BMBaseLibrary/WeexMarketPlugins.h>
-#import <BMBaseLibrary/BMBaseViewController.h>
-#import <BMBaseLibrary/BMNavigationController.h>
-#import <BMBaseLibrary/BMAppResource.h>
 #import <BMBaseLibrary/BMDefine.h>
-#import <BMBaseLibrary/HYGuideView.h>
+
+#ifdef BM_PUSH
+#import <ErosPluginGeTui/BMPushMessageManager.h>
+#endif
 
 @interface AppDelegate ()
 {
@@ -31,20 +27,18 @@
 
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     
     /** 点击推送消息唤起app时调用方法 */
-    if (TK_PlatformInfo().getui.enabled) {
-        NSDictionary *notificationPayload = launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey];
-        if (notificationPayload) {
-            [BMPushMessageManager addPushNotification:notificationPayload];
-        }
+#ifdef BM_PUSH
+    NSDictionary *notificationPayload = launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey];
+    if (notificationPayload) {
+        [BMPushMessageManager addPushNotification:notificationPayload];
     }
+#endif
     
     if (_isLoad == NO) {
         _isLoad = YES;
@@ -58,55 +52,36 @@
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    if (TK_PlatformInfo().getui.enabled)
-        [[BMPushMessageManager shareInstance] setIsLaunchedByNotification:YES];
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    if (TK_PlatformInfo().getui.enabled)
-        [[BMPushMessageManager shareInstance] setIsLaunchedByNotification:NO];
-    
     [[UIApplication sharedApplication] cancelAllLocalNotifications];
     [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
 }
 
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
     if (_isLoad == NO) {
-        _isLoad = YES;
-        
         [self startApp];
     }
-    
+#ifdef BM_PUSH
+    [[BMPushMessageManager shareInstance] setIsLaunchedByNotification:NO];
+#endif
     return [BMRouterManager application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
 }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
+#ifdef BM_PUSH
     /* 获取到deviceToken */
-    if (TK_PlatformInfo().getui.enabled)
-        [BMPushMessageManager registerForRemoteNotificationsWithDeviceToken:deviceToken];
+    [BMPushMessageManager registerForRemoteNotificationsWithDeviceToken:deviceToken];
+#endif
 }
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
@@ -117,30 +92,29 @@
 
 - (void)application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
-    if (TK_PlatformInfo().getui.enabled)
-        [BMPushMessageManager performFetchWithCompletionHandler:completionHandler];
+#ifdef BM_PUSH
+    [BMPushMessageManager performFetchWithCompletionHandler:completionHandler];
+#endif
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
     
+#ifdef BM_PUSH
     /* 收到push消息 */
-    if (TK_PlatformInfo().getui.enabled)
-        [BMPushMessageManager receiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+    [BMPushMessageManager receiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+#endif
 }
 
 -(void)startApp
 {
+    _isLoad = YES;
+    
     /* 初始化数据 */
     [BMConfigManager configDefaultData];
     
-    /* 初始化push服务 */
-    if (TK_PlatformInfo().getui.enabled)
-        [BMPushMessageManager configPushService];
-    
     /** 加载页面 */
-    [[BMMediatorManager shareInstance] loadViewControllerWithWindow:_window];
+    [[BMMediatorManager shareInstance] loadViewControllerWithWindow:self.window];
     
 }
-
 
 @end
