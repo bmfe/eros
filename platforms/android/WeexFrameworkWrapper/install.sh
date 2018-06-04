a00747a121aeb9247ac5d1fd4001e4c8f5a1dd3b
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-06-04 15:30:45 +0800

    commit install.sh

--- platforms/android/WeexFrameworkWrapper/install.sh
+++ platforms/android/WeexFrameworkWrapper/install.sh
@@ -1,24 +1,44 @@
 #!/usr/bin/env bash
-echo "开始更新依赖库"
-rm -r wxframework/
-rm -r sdk/
-rm -r nexus/
-rm -r ErosPluginAmap/
-rm -r erosplugingt/
-rm -r erospluginwxpay/
-rm -r erospluginumeng/
 
-git clone https://github.com/bmfe/WeexErosFramework.git "wxframework"
+echo "执行 git submodule update --init --recursive "
+git submodule update --init --recursive
 
-git clone https://github.com/bmfe/eros-nexus.git "nexus"
+echo "切换主分支"
+# git clone https://github.com/bmfe/WeexErosFramework.git "wxframework"
 
-git clone https://github.com/bmfe/eros-plugin-android-amap.git "ErosPluginAmap"
+# git clone https://github.com/bmfe/eros-nexus.git "nexus"
 
-git clone https://github.com/bmfe/eros-plugin-android-getui.git "erosplugingt"
+# git clone https://github.com/bmfe/eros-plugin-android-amap.git "ErosPluginAmap"
 
-git clone https://github.com/bmfe/eros-plugin-android-wxpay.git "erospluginwxpay"
+# git clone https://github.com/bmfe/eros-plugin-android-getui.git "erosplugingt"
 
-git clone https://github.com/bmfe/eros-plugin-android-wxshare.git "erospluginumeng"
+# git clone https://github.com/bmfe/eros-plugin-android-wxpay.git "erospluginwxpay"
+
+# git clone https://github.com/bmfe/eros-plugin-android-wxshare.git "erospluginumeng"
+
+cd wxframework
+git checkout master
 cd ../
+
+cd nexus
+git checkout master
+cd ../
+
+cd ErosPluginAmap
+git checkout master
+cd ../
+
+cd erosplugingt
+git checkout master
+cd ../
+
+cd erospluginwxpay
+git checkout master
+cd ../
+
+cd erospluginumeng
+git checkout master
+cd ../
+
 
 echo "依赖库更新完成"
