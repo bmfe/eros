ccfb04ac7107f26207ceb0a47c3c98d9ab3c487a
xionghuayu <xionghuayu@benmu-health.com>
2018-09-26 17:05:27 +0800

    update

--- platforms/ios/WeexEros/WeexEros/main.m
+++ platforms/ios/WeexEros/WeexEros/main.m
@@ -3,7 +3,7 @@
 //  WeexEros
 //
 //  Created by XHY on 2017/8/7.
-//  Copyright © 2017年 benmu. All rights reserved.
+//  Copyright © 2017年 Byte Master. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
